32a116f95c2694ac0a226d17e49ce9087a598aa4
Daichi <daichi@murokami.com>
2023-12-09 01:37:12 -0800

    arm target distances now scale with distance from headset - TODO calibrate a neck point on startup instead of using headset

--- cockpit_test/arm_target.gd
+++ cockpit_test/arm_target.gd
@@ -1,28 +1,30 @@
+# TODO make arm the base node
+# als0 this messy
+
 
 class_name ArmTarget
 extends RigidBody3D
 
 
-@export var control_hand : Node3D
+@export var control_hand : HandleArms
 
 # deprec
 @export var control_origin : Node3D 
 @export var control_scaling = 2.0
 
-@export var lerp_amount = 0.0
+@export var lerp_amount = 20.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Label3D.text = "ready"
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var hand_disp = control_origin.to_local(control_hand.global_position)
-	var target = control_origin.global_position + hand_disp * control_scaling
-	
-	global_position = global_position.lerp(target, delta * lerp_amount)
-	rotation = rotation.lerp(control_hand.rotation, delta * lerp_amount)
-
+func _physics_process(delta):
+	var target = control_hand.mech_target.global_transform
+	$Label3D.text = str(target)
+	global_transform = global_transform.interpolate_with(target, lerp_amount * delta)
+	#$Label3D.text = "target"
 
