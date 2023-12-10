b64858d61e6a834b78c94b1110769a553ef11d85
Daichi <daichi@murokami.com>
2023-12-10 01:51:07 -0800

    whee

--- android/build/assets/gameplay/mechbody/flight_module.gd
+++ android/build/assets/gameplay/mechbody/flight_module.gd
@@ -8,22 +8,28 @@ extends Node3D
 
 @export var mechbody : RigidBody3D
 @export var coefficient = 1.2
+@export var max_aoa = PI / 2
 @export var enabled = false
-
-var fore = global_transform.basis.z
 
 
 func _physics_process(delta):
 	if !enabled: return
 	
+	var fore = -global_transform.basis.z
 	var vel = mechbody.linear_velocity
-	var alpha = fore.angle_to(vel)
+	var alpha = fore.signed_angle_to(vel, -global_transform.basis.x)
 	
-	var projected = vel.project(fore)
-	var up = vel - projected.normalized()
+	if alpha > max_aoa: 
+		$Label3D3.text = "stalls"
+		return
+	if vel.dot(fore) < 0: 
+		$Label3D3.text = "vectors opposite"
+		return 
 	
-	var lift = up * alpha * coefficient
+	var lift = global_transform.basis.y * alpha * coefficient * vel.length_squared()
 	mechbody.apply_central_force(lift)
 	
+	var length = lift.length()
+	$Label3D3.text = str(length)
 	
 	
